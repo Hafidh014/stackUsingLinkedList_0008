d6fdfb224f045d107ce6d0054592b06ed1f9a1f3
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-14 15:06:43 +0700

    implementasi metode peek

--- stack.cpp
+++ stack.cpp
@@ -49,6 +49,19 @@ class stack
 
     void peek()
     {
-
+                if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
     }
 };
